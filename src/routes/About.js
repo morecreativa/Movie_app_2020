07c664b273b70369f76db47395af9f34ba4e8a4e
morecreativa <59593893+morecreativa@users.noreply.github.com>
2020-07-31 02:50:27 +0900

    Final

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About(){
+    return (
+        <div className="about__container">
+            <span>
+                "No one does when they begin. Ideas don't come out fully formed. They only become clear as you work on them. Let's start."
+            </span>
+            <span>- Mark Zuckerberg, 2013</span>
+        </div>
+    );
+}
+
+export default About;
